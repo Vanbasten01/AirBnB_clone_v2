d201d6e0bb2dd139c3a630e9cef8edf8d55cced0
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-20 13:57:52 +0100

    tst

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,7 +58,7 @@ class DBStorage:
         """reload method"""
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine,
-                expire_on_commit=False)
+                               expire_on_commit=False)
         self.__session = scoped_session(Session)
 
     def close(self):
